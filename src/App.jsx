f2e6b4ddcd09ee814bf1e8b6a78f12ae79bb6ac7
Denis S Dujota <denisdujota@gmail.com>
2024-08-13 17:38:56 +0300

    update the sign out functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-// App.jsx
-
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import authService from './services/authService';
+
+// Components
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
+import SigninForm from './components/SigninForm/SigninForm'
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(authService.getUser());
+
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  }
 
   return (
     <>
-      <NavBar user={user} />
+      <NavBar user={user} handleSignout={handleSignout}/>
       <Routes>
         { user ? (
           <Route path="/" element={<Dashboard user={user} />} />
@@ -20,6 +27,8 @@ const App = () => {
           <Route path="/" element={<Landing />} />
         )}
         <Route path="/signup" element={<SignupForm setUser={setUser}/>} />
+        <Route path="/signin" element={<SigninForm setUser={setUser}/>} />
+
       </Routes>
     </>
   );
